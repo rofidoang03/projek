c6fd1ce3180d2f71b8c211278eaa648440047a2a
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-31 19:42:59 +0700

    Update spoof.py

--- spoof.py
+++ spoof.py
@@ -8,15 +8,24 @@ def handle_packet(pkt):
         ssid = pkt[Dot11Elt].info.decode()
         bssid = pkt[Dot11].addr3
         channel = int(ord(pkt[Dot11Elt:3].info))
-        network_info = (ssid, bssid, channel)
+        capabilities = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}"
+                                   "{Dot11ProbeResp:%Dot11ProbeResp.cap%}")
+
+        auth_type = None
+        if capabilities is not None and 'privacy' in capabilities:
+            auth_type = "WPA/WPA2"
+        else:
+            auth_type = "Open"
+
+        network_info = (ssid, bssid, channel, auth_type)
         if network_info not in found_networks:
             found_networks.append(network_info)
-            print(f"SSID: {ssid}, BSSID: {bssid}, Channel: {channel}")
+            print(f"SSID: {ssid}, BSSID: {bssid}, Channel: {channel}, Auth: {auth_type}")
 
 def find_wifi_networks(interface):
     for channel in range(1, 14):
         os.system(f"iwconfig {interface} channel {channel}")
-        # print(f"Scanning on channel {channel}...")
+        print(f"Scanning on channel {channel}...")
         sniff(iface=interface, prn=handle_packet, timeout=5)
 
 # Ganti "wlan0" dengan interface wireless Anda
