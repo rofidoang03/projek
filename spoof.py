78625de4dbc07224e07d9d5ac8c58e77a93f17cb
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-31 19:28:38 +0700

    Update spoof.py

--- spoof.py
+++ spoof.py
@@ -1,11 +1,16 @@
 from scapy.all import *
 
+# List untuk menyimpan SSID yang sudah ditemukan
+found_ssids = []
+
 # Fungsi untuk menangani paket Beacon (untuk menemukan jaringan Wi-Fi)
 def handle_packet(pkt):
     if pkt.haslayer(Dot11Beacon):
         ssid = pkt[Dot11Elt].info.decode()
         bssid = pkt[Dot11].addr3
-        print(f"SSID: {ssid}, BSSID: {bssid}")
+        if ssid not in found_ssids:
+            found_ssids.append(ssid)
+            print(f"SSID: {ssid}, BSSID: {bssid}")
 
 # Memulai sniffing pada interface wireless
 def find_wifi_networks(interface):
